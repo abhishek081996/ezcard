46e3b52d941e741c4349f4c9b72b2cd653d7bcd0
Your Name <you@example.com>
2024-05-16 22:55:25 +0530

    added services 1

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import './Services.scss';
+import onboarding from '../../Assets/CustomerOnboarding.png'
 function Services()
 {
 return <>
@@ -9,6 +10,25 @@ return <>
     <Row>
         <Col></Col>
         <Col className='plb'>Payments. Lending.<br/>Banking.</Col>
+        </Row>
+        <Row className='onboarding'>
+            <Col>
+            <img src={onboarding}></img>
+            </Col>
+            <Col>
+            <div className='innerdiv'>
+                <div>
+            <h1>Customer Onboarding & KYC</h1>
+            <p>We handle opening of user/bank accounts,
+customer onboarding, compliance checks, and
+regulatory requirements, end-to-end. We
+guarantee industry standard compliance and a
+frictionless user experience.</p>
+</div>
+<div className='slno'> <p>01</p>
+<div className='bar'></div></div>
+</div>
+            </Col>
         </Row>
     </Container>
 
