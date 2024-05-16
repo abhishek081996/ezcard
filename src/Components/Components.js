f05794c83e48176f7f3928c82a36c8d906b1ba06
Your Name <you@example.com>
2024-05-16 16:01:07 +0530

    excard

---
+++ src/Components/Components.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './LandingPage/LandingPage';
+import Services from './Services/Services';
+function Components ()
+{
+    return (<div>
+        <LandingPage/>
+        <Services/>
+    </div>);
+}
+export default Components;
+
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './Services.scss';
+function Services()
+{
+return <>
+<div className='services'>
+    <Container fluid>
+    <Row>
+        <Col></Col>
+        <Col className='plb'>Payments. Lending.<br/>Banking.</Col>
+        </Row>
+    </Container>
+
+</div>
+</>
+}
+
+export default Services;
